b8dfb8806e359ed82ed2a8582bf55f5e247cd6f9
Kavindu0096 <kavindunimasara@gmail.com>
2020-01-29 16:53:25 +0530

    pakaeges

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get("/api/heroes", (req, res) => {
 app.get("/api/heroes/:heroId", (req, res) => {
     let userRequestedId = req.params.heroId;
     //res.send("You have requested ");
-    let requestedHero = heroArray.find(h => h.id ===  "1");
+    let requestedHero = heroArray.find(h => h.id === "1");
     let test = heroArray.find(h => h.id === 1)
     res.send(requestedHero);
     console.log(requestedHero);
@@ -33,21 +33,21 @@ app.get("/api/heroes/:heroId", (req, res) => {
 });
 
 //POST
-app.post("/api/heroes", (req,  res) => {
+app.post("/api/heroes", (req, res) => {
     //Read the req body and find all the values of the new object to be created 
     //Create the object
     //Append it to the array 
     console.log(req.body);
 
-    if(!req.body.name){
+    if (!req.body.name) {
         res.status(400).send("Please check name");
         return;
     }
     let newHero = {
-        id : heroArray.length + 1,
-        name : req.body.name,
-        SuperPower : req.body.SuperPower,
-        likeCount : req.body.likeCount 
+        id: heroArray.length + 1,
+        name: req.body.name,
+        SuperPower: req.body.SuperPower,
+        likeCount: req.body.likeCount
     }
     heroArray.push(newHero);
     res.send(newHero);
@@ -56,37 +56,37 @@ app.post("/api/heroes", (req,  res) => {
 
 
 //PUT
-app.put("/api/heroes/:heroId", (req,  res) => {
-    let requestedIdtoEdit = parseInt(req.params.heroId);    
-    if(!req.body.likeCount){
+app.put("/api/heroes/:heroId", (req, res) => {
+    let requestedIdtoEdit = parseInt(req.params.heroId);
+    if (!req.body.likeCount) {
         return res.status(400).send("Please check values");
     }
-    
-    let heroToEdit = heroArray.find(h => h.id ===  requestedIdtoEdit);
 
-    if(!heroToEdit){
+    let heroToEdit = heroArray.find(h => h.id === requestedIdtoEdit);
+
+    if (!heroToEdit) {
         return res.status(404).send("Given id does not exist");
     }
-   
+
     heroToEdit.likeCount = req.body.likeCount;
     res.send(heroToEdit);
-    
+
 
 });
 
 
 //DELETE
-app.delete("/api/heroes/:heroId", (req,  res) => {
-    let requestedIdtoDelete = parseInt(req.params.heroId);    
-    let heroToDelete = heroArray.find(h => h.id ===  requestedIdtoDelete);
-    if(!heroToDelete){
+app.delete("/api/heroes/:heroId", (req, res) => {
+    let requestedIdtoDelete = parseInt(req.params.heroId);
+    let heroToDelete = heroArray.find(h => h.id === requestedIdtoDelete);
+    if (!heroToDelete) {
         return res.status(404).send("Given id does not exist");
     }
     let indexOfHero = heroArray.indexOf(heroToDelete)
     heroArray.splice(indexOfHero, 1);
 
     res.send(heroToDelete);
-    
+
 
 });
 
